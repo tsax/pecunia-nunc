e4941283101283f4770e5978b9f19c856486886d
Tushar S <tsaxena86@gmail.com>
2013-03-21 01:26:16 -0400

    add rspec-rails, install rspec

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,2 +1,11 @@
 class SubscribersController < ApplicationController
+
+	def new
+		@subscriber = Subscriber.new(params[:subscriber])
+		if @subscriber.save
+			flash[:notice] = "Thanks for signing-up. Please confirm your email address through the email that'll be in your inbox shortly!"
+		else
+			flash[:notice] = "There were errors! Please resubmit after making corrections."
+		end	
+	end
 end
