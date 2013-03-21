26db5925b54922d32263fc6094e91a320d35da63
Tushar S <tsaxena86@gmail.com>
2013-03-21 17:34:58 -0400

    add 'new' action on subscribers controller

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -4,6 +4,7 @@ class SubscribersController < ApplicationController
 		@subscriber = Subscriber.new(params[:subscriber])
 		if @subscriber.save
 			flash[:notice] = "Thanks for signing-up. Please confirm your email address through the email that'll be in your inbox shortly!"
+			redirect_to home_path
 		else
 			flash[:notice] = "There were errors! Please resubmit after making corrections."
 		end	
