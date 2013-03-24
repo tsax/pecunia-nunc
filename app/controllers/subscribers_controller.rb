56137098fb04af8ce24786d492e845496d1e519a
Tushar S <tsaxena86@gmail.com>
2013-03-24 19:54:59 -0400

    add @subscriber.active = true

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -10,7 +10,7 @@ class SubscribersController < ApplicationController
 		@subscriber.token = Digest::SHA1.hexdigest([Time.now, rand].join)
 		if @subscriber.save
 			flash[:notice] = "Thanks for signing-up. Please confirm your email address through the email that'll be in your inbox shortly!"
-			
+
 			redirect_to home_path
 		else
 			flash[:notice] = "There were errors! Please resubmit after making corrections."
@@ -23,11 +23,13 @@ class SubscribersController < ApplicationController
 		# this action. Check params[:token] against token, if it
 		# matches, set active boolean of the record to true,
 		# display a flash notice of 'thanks your email is confirmed'
-		if Subscriber.find_by_token(params[:token]).nil?
+		@subscriber = Subscriber.find_by_token(params[:token])
+		if @subscriber.nil?
 			flash[:notice] = 'Sorry your token did not match
 												our records. Please re-subscribe or try
 												re-clicking the Confirm link in your email'
 		else
+			@subscriber.active = true
 			flash[:notice] = 'Thank you! Your email address has been
 												confirmed! Expect your daily email to arrive
 												everyday'
