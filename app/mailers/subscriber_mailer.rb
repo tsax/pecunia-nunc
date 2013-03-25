769db3704980142a516102ac882cc956afce3960
Tushar S <tsaxena86@gmail.com>
2013-03-25 18:01:43 -0400

    mailer->daily_email

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -3,6 +3,12 @@ class SubscriberMailer < ActionMailer::Base
 
   def confirmation_mail(subscriber)
   	@subscriber = subscriber
-  	@url = "#{request.fullpath.split("?")[0]}"
+  	@url = "#{request.fullpath.split("?")[0]}?do=confirm&&token=#{@subscriber.token}"
+  end
+
+  def daily_email(subscriber, projects)
+  	@subscriber = subscriber
+  	@projects = projects
+  	@url = "#{request.fullpath.split("?")[0]}?do=unsubscribe&&token=#{@subscriber.token}"
   end
 end
