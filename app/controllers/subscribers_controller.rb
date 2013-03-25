772ebf50b4da757b0172ff7a56b42a0372e55ef4
Tushar S <tsaxena86@gmail.com>
2013-03-25 01:11:46 -0400

    replace redirect_to with render in subscribers#new

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -14,24 +14,20 @@ class SubscribersController < ApplicationController
 			redirect_to home_path
 		else
 			flash[:notice] = "There were errors! Please resubmit after making corrections."
-			redirect_to home_path
+			render 'home'
 		end	
 	end
 
 	def confirm_or_unsubscribe
 		@subscriber = Subscriber.find_by_token(params[:token])
-		Rails.env.development? and binding.pry
+		binding.pry if Rails.env.development?
 		if @subscriber.nil?
-			flash[:notice] = 'Sorry your token did not match
-												our records. Please re-subscribe or try
-												re-clicking the Confirm link in your email'
+			flash[:notice] = 'Sorry your token did not match our records. Please re-subscribe or try re-clicking the Confirm link in your email'
 		  redirect_to home_path
 		elsif params[:do] == 'confirm'
 			@subscriber.active = true
 			@subscriber.save
-			flash[:notice] = 'Thank you! Your email address has been
-												confirmed! Expect your daily email to arrive
-												everyday'
+			flash[:notice] = 'Thank you! Your email address has been confirmed! Expect your daily email to arrive everyday'
       puts 'Confirmed'
 			redirect_to home_path
 	  
