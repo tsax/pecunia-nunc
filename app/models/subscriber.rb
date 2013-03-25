5883e60387ed2e4e7842f551875640d5850336a3
Tushar S <tsaxena86@gmail.com>
2013-03-24 20:41:44 -0400

    modify validates_presence_of :token to validates

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -8,5 +8,6 @@ class Subscriber < ActiveRecord::Base
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates_uniqueness_of :token
+  validates :token, presence: false,
+  									uniqueness: {case_sensitive: true }
 end
