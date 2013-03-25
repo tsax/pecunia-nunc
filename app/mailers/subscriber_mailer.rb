9b6a0498f869af2229efae505dec2366c2718909
Tushar S <tsaxena86@gmail.com>
2013-03-25 18:15:02 -0400

    add mail to: in daily and reminder email methods

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -4,11 +4,13 @@ class SubscriberMailer < ActionMailer::Base
   def confirmation_mail(subscriber)
   	@subscriber = subscriber
   	@url = "#{request.fullpath.split("?")[0]}?do=confirm&&token=#{@subscriber.token}"
+  	mail to: @subscriber.email, subject: "Welcome to Pecunia Nunc. Please confirm this address"
   end
 
   def daily_email(subscriber, projects)
   	@subscriber = subscriber
   	@projects = projects
   	@url = "#{request.fullpath.split("?")[0]}?do=unsubscribe&&token=#{@subscriber.token}"
+  	mail to: @subscriber.email, subject: "Kickstarters Ending Today"
   end
 end
